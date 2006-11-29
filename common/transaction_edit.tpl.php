7c1491d62f84c599819be2127817f1d63f4781da
hunterjensen <hunterjensen@a1e15182-8120-0410-838e-b39dc8dad89f>
2006-11-29 19:33:40 +0000

    "Fixed copyright notice on all /common files."

--- common/transaction_edit.tpl.php
+++ common/transaction_edit.tpl.php
@@ -18,9 +18,7 @@
  * along with Tracmor; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-?>
 
-<?php
 	include('../includes/header.inc.php');
 	$this->RenderBegin();
 ?>
