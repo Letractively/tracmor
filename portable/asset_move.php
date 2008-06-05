8fcefa30f6491a800e200b5d31c2417703ae9335
alexander.klyutchenya <alexander.klyutchenya@moveyourweb.net>
2008-06-05 18:41:06 +0000

    Fixed JS-bugs and PHP-errors in Move Assets

--- portable/asset_move.php
+++ portable/asset_move.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['intUserAccountId'])) {
 }
 else QApplication::$objUserAccount = UserAccount::Load($_SESSION['intUserAccountId']);
 
-$warning = "";
+$strWarning = "";
 
 if ($_POST && $_POST['method'] == 'complete_transaction') {
 	/*
@@ -35,30 +35,30 @@ if ($_POST && $_POST['method'] == 'complete_transaction') {
 				$objNewAsset = Asset::LoadByAssetCode($strAssetCode);
 				if (!($objNewAsset instanceof Asset)) {
 					$blnError = true;
-					$warning = "That asset code does not exist.";
+					$strWarning = "That asset code does not exist.";
 				}				
 				// Cannot move, check out/in, nor reserve/unreserve any assets that have been shipped
 				elseif ($objNewAsset->LocationId == 2) {
 					$blnError = true;
-					$warning = "That asset has already been shipped.";
+					$strWarning = "That asset has already been shipped.";
 				}
 				// Cannot move, check out/in, nor reserve/unreserve any assets that are scheduled to  be received
 				elseif ($objNewAsset->LocationId == 5) {
 					$blnError = true;
-					$warning = "That asset is currently scheduled to be received.";
+					$strWarning = "That asset is currently scheduled to be received.";
 				}
 				elseif ($objPendingShipment = AssetTransaction::PendingShipment($objNewAsset->AssetId)) {
 					$blnError = true;
-					$warning = "That asset is already in a pending shipment.";
+					$strWarning = "That asset is already in a pending shipment.";
 				}
 				// Move
 				elseif ($objNewAsset->CheckedOutFlag) {
 					$blnError = true;
-					$warning = "That asset is checked out.";
+					$strWarning = "That asset is checked out.";
 				}
 				elseif ($objNewAsset->ReservedFlag) {
 					$blnError = true;
-					$warning = "That asset is reserved.";
+					$strWarning = "That asset is reserved.";
 				}
 				
 				/*if (!$blnError && $objNewAsset instanceof Asset)  {
@@ -67,37 +67,43 @@ if ($_POST && $_POST['method'] == 'complete_transaction') {
 			}
 		}
 		else {
-			$warning = "Please enter an asset code.";
+			$strWarning = "Please enter an asset code.";
 		}
 	}
 	
 	if (!$blnError) {
-	    //!!!Note: LoadByShortDescription returns Location but integer, so we have error
-		$intDestinationLocationId = Location::LoadByShortDescription($_POST['destination_location']);
-	    $arrAssetCode = explode('#',$_POST['result']);
-	    
-	    foreach ($arrAssetCode as $strAssetCode) {
-			
-			$objAsset = Asset::LoadByAssetCode($strAssetCode);
-			$objTransaction = new Transaction();
-			$objTransaction->EntityQtypeId = EntityQtype::Asset;
-			$objTransaction->TransactionTypeId = 1; // Move
-			$objTransaction->Save();
-			
-			$objAssetTransaction = new AssetTransaction();
-			$objAssetTransaction->AssetId = $objAsset->AssetId;
-			$objAssetTransaction->TransactionId = $objTransaction->TransactionId;
-			$objAssetTransaction->SourceLocationId = $objAsset->LocationId;
-			$objAssetTransaction->DestinationLocationId = $intDestinationLocationId;
-			$objAssetTransaction->Save();
-			
-			$objAsset->LocationId = $intDestinationLocationId;
-			$objAsset->Save();
-		}
-		$warning = "Your transaction has successfully completed";
+	    $objDestinationLocation = Location::LoadByShortDescription($_POST['destination_location']);
+        if (!$objDestinationLocation) {
+            $blnError = true;
+            $strWarning = "Detination Location does not exist.";
+        }
+        else {
+    	    $intDestinationLocationId = $objDestinationLocation->LocationId;
+    	    $arrAssetCode = explode('#',$_POST['result']);
+    	    
+    	    foreach ($arrAssetCode as $strAssetCode) {
+    			
+    			$objAsset = Asset::LoadByAssetCode($strAssetCode);
+    			$objTransaction = new Transaction();
+    			$objTransaction->EntityQtypeId = EntityQtype::Asset;
+    			$objTransaction->TransactionTypeId = 1; // Move
+    			$objTransaction->Save();
+    			
+    			$objAssetTransaction = new AssetTransaction();
+    			$objAssetTransaction->AssetId = $objAsset->AssetId;
+    			$objAssetTransaction->TransactionId = $objTransaction->TransactionId;
+    			$objAssetTransaction->SourceLocationId = $objAsset->LocationId;
+    			$objAssetTransaction->DestinationLocationId = $intDestinationLocationId;
+    			$objAssetTransaction->Save();
+    			
+    			$objAsset->LocationId = $intDestinationLocationId;
+    			$objAsset->Save();
+    		}
+    		$strWarning = "Your transaction has successfully completed<br/><a href='index.php'>Main Menu</a> | <a href='asset_menu.php'>Manage Assets</a>";
+    		//Remove that flag when transaction is compelete or exists some errors
+            unset($_SESSION['intUserAccountId']);
+        }
 	}
-	//Remove that flag when transaction is compelete or exists some errors
-    unset($_SESSION['intUserAccountId']);
 }
 
 ?>
@@ -113,11 +119,8 @@ function AddAsset() {
     strAssetCode = document.getElementById('asset_code').value;
     if (strAssetCode != '') {
         document.getElementById('warning').innerHTML = "";
-        if (!isNaN(parseInt(strAssetCode))) { 
-            arrayAssetCode[i] = parseInt(strAssetCode);
-            document.getElementById('result').innerHTML += arrayAssetCode[i++] + "<br/>";
-        }
-        else document.getElementById('warning').innerHTML = "Asset Code must contain only digits";
+        arrayAssetCode[i] = strAssetCode;
+        document.getElementById('result').innerHTML += arrayAssetCode[i++] + "<br/>";
         document.getElementById('asset_code').value = '';
         document.getElementById('asset_code').focus();
     }
@@ -130,7 +133,15 @@ function AddAsset() {
 function CompleteMove() {
     var strAssetCode = "";
     strAssetCode = arrayAssetCode.join("#");
-    if (strAssetCode != "" && document.main_form.destination_location.value != "") {
+    if (arrayAssetCode.length == 0) {
+        document.getElementById('warning').innerHTML = "You must provide at least one asset";
+        return false;
+    }
+    if (document.main_form.destination_location.value == "") {
+        document.getElementById('warning').innerHTML = "Destination Location cannot be empty";
+        return false;
+    }
+    if (arrayAssetCode.length>0 && document.main_form.destination_location.value != "") {
          document.main_form.result.value = strAssetCode;
          document.main_form.submit();
     }
@@ -141,17 +152,17 @@ function CompleteMove() {
 
 <h1>TRACMOR PORTABLE INTERFACE</h1>
 <h3>Move Assets</h3>
-Asset Code: <input type="text" id="asset_code" size="10">
+<div id="warning"><? echo $strWarning; ?></div>
+Asset Code: <input type="text" id="asset_code" onkeyup="javascript:if(event.keyCode=='13') AddAsset();" size="10">
 <input type="button" value="Add Asset" onclick="javascript:AddAsset();">
-<div id="warning"><? echo $warning; ?></div>
 <div id="result"></div>
 <!-- When the user clicks Add Asset, we need to add that asset code to an array using Javascript to prepare for submitting the transaction -->
 <br /><br />
-<form method="post" name="main_form">
+<form method="post" name="main_form" onsubmit="javascript:CompleteMove();">
 <input type="hidden" name="method" value="complete_transaction">
 <input type="hidden" name="result" value="">
 Destination Location: <input type="text" name="destination_location" size ="20">
-<input type="button" value="Complete Move" onclick="javascript:CompleteMove();">
+<input type="button" value="Complete Move" onclick="javascript:CompleteMove();" on>
 </form>
 
 </body>
