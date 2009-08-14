81eeb91941045bb2c1def663a6770a81b7072780
jsinclair <jsinclair@a1e15182-8120-0410-838e-b39dc8dad89f>
2009-08-14 18:11:23 +0000

    Data model changes for Thermal Label Print.

---
+++ data_model/ThermalLabelPrint_update-01.08.00.sql
@@ -0,0 +1,15 @@
+ALTER TABLE  `fedex_shipment` ADD  `label_printer_type` INT NULL ,
+ADD  `label_format_type` INT NULL ,
+ADD  `thermal_printer_port` VARCHAR( 255 ) NULL;
+
+INSERT INTO  `tracmor`.`admin_setting` (
+	`short_description` ,
+	`value`
+)
+VALUES (
+	'fedex_label_printer_type',  '1'
+), (
+	'fedex_label_format_type',  '5'
+), (
+	'fedex_thermal_printer_port', 'LPT1'
+);
