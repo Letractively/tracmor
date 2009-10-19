a984a92e7b05c19040e9c84750e7c5824bbff6c0
hunterjensen <hunterjensen@a1e15182-8120-0410-838e-b39dc8dad89f>
2009-10-19 22:16:30 +0000

    "cleaning custom_field_selection table"

--- data_model/AssetImport_update-01.08.00.sql
+++ data_model/AssetImport_update-01.08.00.sql
@@ -5,3 +5,7 @@ INSERT INTO `admin_setting` (
 VALUES (
 	'asset_limit', NULL
 );
+
+DELETE `custom_field_value`.* FROM `custom_field_value` LEFT JOIN `custom_field` ON `custom_field_value`.`custom_field_id` = `custom_field`.`custom_field_id` WHERE `custom_field`.`custom_field_qtype_id` != 2;
+
+DROP TABLE `custom_field_selection`;
