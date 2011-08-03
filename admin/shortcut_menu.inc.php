f832c22fa78053ab183fc3ada7b66eeac42d865f
jsinclair <jsinclair@gmail.com>
2011-08-03 17:20:30 +0000

    Merged trunk changes into branch

--- admin/shortcut_menu.inc.php
+++ admin/shortcut_menu.inc.php
@@ -40,20 +40,20 @@
 								<td style="border-left:1px solid #CCCCCC;border-bottom:1px solid #CCCCCC;padding-left:5px;cursor:pointer;" width="100%" onmouseover="this.style.backgroundColor='#EEEEEE';" onmouseout="this.style.backgroundColor='#FFFFFF';"><a href="index.php" class="graylink">Settings</a></td>
 							</tr>
 							<tr>
-								<td width="30" align="center"><img src="../images/icons/checkin_out.png" name="shortcut1"></td>
-								<td style="border-left:1px solid #CCCCCC;border-bottom:1px solid #CCCCCC;padding-left:5px;cursor:pointer;" width="100%" onmouseover="this.style.backgroundColor='#EEEEEE';" onmouseout="this.style.backgroundColor='#FFFFFF';"><a href="asset_check_out_in.php" class="graylink">Check In/Out</a></td>
-							</tr>
-							<tr>
-								<td width="30" align="center"><img src="../images/icons/category.png" name="shortcut2"></td>
+								<td width="30" align="center"><img src="../images/icons/category.png" name="shortcut1"></td>
 								<td style="border-left:1px solid #CCCCCC;border-bottom:1px solid #CCCCCC;padding-left:5px;cursor:pointer;" width="100%" onmouseover="this.style.backgroundColor='#EEEEEE';" onmouseout="this.style.backgroundColor='#FFFFFF';"><a href="category_list.php" class="graylink">Categories</a></td>
 							</tr>
 							<tr>
-								<td width="30" align="center"><img src="../images/icons/manufacturers.png" name="shortcut3"></td>
+								<td width="30" align="center"><img src="../images/icons/manufacturers.png" name="shortcut2"></td>
 								<td style="border-left:1px solid #CCCCCC;border-bottom:1px solid #CCCCCC;padding-left:5px;cursor:pointer;" width="100%" onmouseover="this.style.backgroundColor='#EEEEEE';" onmouseout="this.style.backgroundColor='#FFFFFF';"><a href="manufacturer_list.php" class="graylink">Manufacturers</a></td>
 							</tr>
 							<tr>
-								<td width="30" align="center"><img src="../images/icons/locations.png" name="shortcut4"></td>
+								<td width="30" align="center"><img src="../images/icons/locations.png" name="shortcut3"></td>
 								<td style="border-left:1px solid #CCCCCC;border-bottom:1px solid #CCCCCC;padding-left:5px;cursor:pointer;" width="100%" onmouseover="this.style.backgroundColor='#EEEEEE';" onmouseout="this.style.backgroundColor='#FFFFFF';"><a href="location_list.php" class="graylink">Locations</a></td>
+							</tr>
+							<tr>
+								<td width="30" align="center"><img src="../images/icons/checkin_out.png" name="shortcut4"></td>
+								<td style="border-left:1px solid #CCCCCC;border-bottom:1px solid #CCCCCC;padding-left:5px;cursor:pointer;" width="100%" onmouseover="this.style.backgroundColor='#EEEEEE';" onmouseout="this.style.backgroundColor='#FFFFFF';"><a href="asset_check_out_in.php" class="graylink">Check In/Out</a></td>
 							</tr>
 							<tr>
 								<td width="30" align="center"><img src="../images/icons/shipping_and_receiving.png" name="shortcut5"></td>
